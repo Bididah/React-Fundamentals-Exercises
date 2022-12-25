13ae94d96c5cf02af8ab507c54c4cab491aa2c85
Hamza hanfi <hanfihamza20@gmail.com>
2022-12-25 18:25:13 +0100

    seting the likes button

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,18 @@
 import corgi from "./corgi.jpg";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [likes, setLikes] = useState(0)
+
+  const incrementLikes = () => setLikes(likes + 1)
+
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <p>Amount of likes: {`${likes} ${likes > 1 ? 'likes' : 'like'}`} </p>
+      <button onClick={() => incrementLikes()}> Like </button>
     </div>
   );
 };
